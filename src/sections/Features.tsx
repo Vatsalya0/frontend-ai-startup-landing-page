d6c755128ce082fe80d87d1c49a62646aa020bf3
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-08-28 15:27:19 +0300

    added a hover animation to features section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { DotLottiePlayer } from "@dotlottie/react-player";
+import {
+  DotLottiePlayer,
+  DotLottieCommonPlayer,
+} from "@dotlottie/react-player";
 import Image from "next/image";
 import productImage from "@/assets/product-image.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const tabs = [
   {
@@ -31,41 +36,59 @@ const tabs = [
   },
 ];
 
+const FeatureTab = (tab: (typeof tabs)[number]) => {
+  const dotlottieRef = useRef<DotLottieCommonPlayer>(null);
+
+  const handleTabHover = () => {
+    if (dotlottieRef.current === null) return;
+    dotlottieRef.current.seek(0);
+    dotlottieRef.current.play();
+  };
+
+  return (
+    <div
+      key={tab.title}
+      onMouseEnter={handleTabHover}
+      className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1"
+    >
+      <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+        <DotLottiePlayer
+          ref={dotlottieRef}
+          src={tab.icon}
+          className="h-5 w-5"
+        />
+      </div>
+      <div className="font-medium"> {tab.title} </div>
+      <div>
+        {tab.isNew && (
+          <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
+            new
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export const Features = () => {
   return (
-    <section className="py-20 md:py-24 ">
+    <section className="py-20 md:py-24">
       <div className="container">
-        <h2 className="text-5xl md:text-6xl  font-medium text-center tracking-tighter">
+        <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">
           Elevate your SEO efforts.
         </h2>
         <p className="text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5">
-          from small startups to large enterprises, our AI-driven tool has
-          revolutionzed the way businesses approach SEO.
+          From small startups to large enterprises, our AI-driven tool has
+          revolutionized the way businesses approach SEO.
         </p>
         <div className="mt-10 flex flex-col lg:flex-row gap-3">
           {tabs.map((tab) => (
-            <div
-              key={tab.title}
-              className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1"
-            >
-              <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
-                <DotLottiePlayer src={tab.icon} className="h-5 w-5" autoplay />
-              </div>
-              <div className="font-medium"> {tab.title} </div>
-              <div>
-                {" "}
-                {tab.isNew && (
-                  <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold ">
-                    new
-                  </div>
-                )}{" "}
-              </div>
-            </div>
+            <FeatureTab {...tab} key={tab.title} />
           ))}
         </div>
-        <div className="border border-white/20 p-2.5 rounded-xl mt-3 ">
+        <div className="border border-white/20 p-2.5 rounded-xl mt-3">
           <div
-            className="aspect-video bg-cover border border-white/20 rounded-lg "
+            className="aspect-video bg-cover border border-white/20 rounded-lg"
             style={{
               backgroundImage: `url(${productImage.src})`,
             }}
