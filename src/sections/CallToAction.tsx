e8b8eabf9a446e2942b72ecfe6c8bdd3ca231f53
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-08-29 09:20:17 +0300

    added call to action section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/button";
+
 export const CallToAction = () => {
-  return <section>CallToAction</section>;
+  return (
+    <section>
+      <div className="contianer">
+        <h2>AI-driven SEO for everyone.</h2>
+        <p>Acheive clear, impactful results without the complexity.</p>
+      </div>
+      <div className="flex gap-4 items-center">
+        <Button>Join waitlist</Button>
+      </div>
+    </section>
+  );
 };
