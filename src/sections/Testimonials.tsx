7813b7ee31217db62ba047db739928b38e753540
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-08-29 08:07:52 +0300

    desgined our testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -33,34 +33,44 @@ const testimonials = [
 
 export const Testimonials = () => {
   return (
-    <section className="py-20">
+    <section className="py-20 md:py-24">
       <div className="container">
-        <h2 className="text-5xl font-medium text-center tracking-tighter">
+        <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">
           Beyond Expectations.
         </h2>
-        <p className="text-white/70 text-lg mt-5 max-w-2xl mx-auto tracking-tight text-center">
+        <p className="text-white/70 text-lg md:text-xl mt-5 tracking-tight text-center max-w-sm mx-auto">
           Our revolutionary AI SEO tools have transformed our client's
           strategies.
         </p>
-        {testimonials.map((testimonial) => (
-          <div
-            key={testimonial.name}
-            className=" border border-white/15 p-6 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,266,.3),black)]"
-          >
-            <div className="text-lg tracking-tight"> {testimonial.text} </div>
-            <div className="flex items-center gap-3 mt-5">
-              <Image
-                className="rounded-lg h-11 w-11 grayscale"
-                src={testimonial.avatarImg}
-                alt={`Avtar for ${testimonial.name}`}
-              />
-              <div>
-                <div> {testimonial.name} </div>
-                <div> {testimonial.title} </div>
+        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <div className="flex gap-5">
+            {testimonials.map((testimonial) => (
+              <div
+                key={testimonial.name}
+                className=" border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,266,.3),black)] max-w-xs md:max-w-md flex-none"
+              >
+                <div className="text-lg md:text-2xl tracking-tight text-white/90 ">
+                  {testimonial.text}
+                </div>
+                <div className="flex items-center gap-3 mt-5">
+                  <div className="relative after-content[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:z-10 before:rounded-lg before:border before:border-white/30 before:inset-0">
+                    <Image
+                      className="rounded-lg h-11 w-11 grayscale "
+                      src={testimonial.avatarImg}
+                      alt={`Avtar for ${testimonial.name}`}
+                    />
+                  </div>
+                  <div>
+                    <div className="text-white/90"> {testimonial.name} </div>
+                    <div className="text-white/50 text-sm ">
+                      {testimonial.title}
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </section>
   );
